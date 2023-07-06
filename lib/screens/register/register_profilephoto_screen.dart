94996fc3551a7ec4bbfa30cd2af0e03f8ccf35a9
urielduana <81336230+urielduana@users.noreply.github.com>
2023-07-06 00:47:19 -0600

    Fixing UI

--- lib/screens/register/register_profilephoto_screen.dart
+++ lib/screens/register/register_profilephoto_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:snapsnap/components/register_appbar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:snapsnap/services/reg.dart';
 import 'package:snapsnap/services/upImg.dart';
 
 class RegisterProfilePhotoScreen extends StatefulWidget {
@@ -86,149 +87,7 @@ class _RegisterProfilePhotoScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: const RegisterAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: [
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text(
-                    "Add a profile picture",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                // Circle with a cupertino icon in the middle of a camera in the center of the screen
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Container(
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).colorScheme.onSurface,
-                          width: 5,
-                        ),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: _selectedImage != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(100),
-                              child: Image.file(
-                                _selectedImage!,
-                                fit: BoxFit.cover,
-                              ),
-                            )
-                          : const Icon(
-                              CupertinoIcons.camera,
-                              size: 75,
-                            )),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 20, left: 25, right: 25),
-                  child: Text(
-                    "Add a profile photo so your friends know it's you",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-                //Button to upload a photo with width of 150
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, left: 50, right: 50),
-                  child: TextButton(
-                    onPressed: () {
-                      _selectAndUpImage();
-                    },
-                    style: TextButton.styleFrom(
-                      minimumSize: const Size.fromHeight(40),
-                      backgroundColor: const Color(0xFF381E72),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      "Upload Photo",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 50),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                          backgroundColor: const Color(0xFF381E72),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 50,
-                            vertical: 20,
-                          ),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                          ),
-                        ),
-                        child: const Text(
-                          "Finish",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        style: TextButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).dividerColor.withOpacity(0.2),
-                          minimumSize: const Size.fromHeight(50),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 50,
-                            vertical: 20,
-                          ),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                          ),
-                        ),
-                        child: const Text("Back"),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      appBar: RegisterAppBar(),
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
@@ -237,7 +96,6 @@ class _RegisterProfilePhotoScreenState
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -261,21 +119,28 @@ class _RegisterProfilePhotoScreenState
                         Padding(
                           padding: const EdgeInsets.only(top: 20),
                           child: Container(
-                            width: 145,
-                            height: 145,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Theme.of(context).colorScheme.onSurface,
-                                width: 5,
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color:
+                                      Theme.of(context).colorScheme.onSurface,
+                                  width: 5,
+                                ),
+                                borderRadius: BorderRadius.circular(100),
                               ),
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Icon(
-                              CupertinoIcons.camera,
-                              size: 60,
-                              color: Theme.of(context).colorScheme.onSurface,
-                            ),
-                          ),
+                              child: _selectedImage != null
+                                  ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(100),
+                                      child: Image.file(
+                                        _selectedImage!,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    )
+                                  : const Icon(
+                                      CupertinoIcons.camera,
+                                      size: 75,
+                                    )),
                         ),
                         const Padding(
                           padding:
@@ -294,7 +159,7 @@ class _RegisterProfilePhotoScreenState
                               top: 20, left: 50, right: 50),
                           child: TextButton(
                             onPressed: () {
-                              // Open gallery
+                              _selectAndUpImage();
                             },
                             style: TextButton.styleFrom(
                               minimumSize: const Size.fromHeight(40),
@@ -322,20 +187,7 @@ class _RegisterProfilePhotoScreenState
                             Padding(
                               padding: const EdgeInsets.only(top: 15),
                               child: TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const RegisterProfilePhotoScreen()));
-                                  // if (_formKey.currentState!.validate()) {
-                                  //   Map data = {
-                                  //     "email": _emailController.text,
-                                  //   };
-                                  //   Provider.of<Register>(context, listen: false)
-                                  //       .verifyEmail(data, context);
-                                  // }
-                                },
+                                onPressed: () {},
                                 style: TextButton.styleFrom(
                                   minimumSize: const Size.fromHeight(50),
                                   backgroundColor: const Color(0xFF381E72),
@@ -350,7 +202,7 @@ class _RegisterProfilePhotoScreenState
                                   ),
                                 ),
                                 child: const Text(
-                                  "Skip",
+                                  "Finish",
                                   style: TextStyle(color: Colors.white),
                                 ),
                               ),
