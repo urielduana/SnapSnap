e6875b6e62853a94b2515cc83feea106b27bd579
urielduana <uri250602@outlook.es>
2023-07-31 10:55:28 -0600

    Registro de tags

--- lib/screens/tags/tag_select.dart
+++ lib/screens/tags/tag_select.dart
@@ -1,7 +1,11 @@
 import 'package:dio/dio.dart' as Dio;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+import 'package:snapsnap/main.dart';
 import 'package:snapsnap/services/dio.dart';
+import 'package:snapsnap/services/reg.dart';
 
 class TagSelectScreen extends StatefulWidget {
   const TagSelectScreen({super.key});
@@ -24,42 +28,117 @@ class _TagSelectScreenState extends State<TagSelectScreen> {
   // Funtion that makes a Dio request to index all tags in the database
   void indexTags() async {
     var token = await storage.read(key: 'token') ?? '';
-    Dio.Response response = await dio().get('/tags',
+    Dio.Response response = await dio().get('/favorite_tags/create',
         options: Dio.Options(headers: {'Authorization': 'Bearer $token'}));
     indexedTags = List<Map<String, dynamic>>.from(response.data);
-    print(indexedTags);
+  }
+
+  void addIdToSelectedTags(int id) {
+    selectedTags.add({'id': id});
+    print(selectedTags);
+    print(selectedTags.isEmpty);
+  }
+
+  void removeIdSelectedTags(int id) {
+    selectedTags.removeWhere((element) => element['id'] == id);
+    print(selectedTags);
+    print(selectedTags.isEmpty);
   }
 
   @override
   Widget build(BuildContext context) {
+    final register = context.watch<Register>();
     // Verifies if the tags have been indexed
-    if (indexedTags == [] || selectedTags == []) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
+    if (indexedTags.isEmpty == true) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Favorite Tags'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                    );
+                  },
+                  child: const Text('Skip',
+                      style: TextStyle(color: Colors.white))),
+            ],
+          ),
+          body: const SafeArea(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ));
     } else {
       return Scaffold(
-          body: SafeArea(
-        // Generates a list with all the content of the indexed tags request
-        child: ListView.builder(
-          itemCount: indexedTags.length,
-          itemBuilder: (BuildContext context, int index) {
-            final tag = indexedTags[index];
-            return ListTile(
-              title: Row(children: [
-                Expanded(
-                  child: Container(
+        // AppBar with the title of the screen and skip button
+        appBar: AppBar(
+          title: const Text('Favorite Tags'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+                child:
+                    const Text('Skip', style: TextStyle(color: Colors.white))),
+          ],
+        ),
+        body: SafeArea(
+          // Generates a list with all the content of the indexed tags request
+          child: ListView.builder(
+            itemCount: indexedTags.length,
+            itemBuilder: (BuildContext context, int index) {
+              final tag = indexedTags[index];
+              return ListTile(
+                title: Row(children: [
+                  Expanded(
                     child: Text(tag['tag_name']),
                   ),
-                ),
-                ElevatedButton(onPressed: () {}, child: Text('Select')),
-              ]),
-            );
-          },
+                  // Change button between add and remove if the tag is selected from the array of objects
+                  // Background color purple if the tag is not selected
+                  if (selectedTags.any((element) => element['id'] == tag['id']))
+                    TextButton(
+                        onPressed: () {
+                          removeIdSelectedTags(tag['id']);
+                          setState(() {});
+                        },
+                        child: const Text('Remove'))
+                  else
+                    TextButton(
+                        onPressed: () {
+                          addIdToSelectedTags(tag['id']);
+                          setState(() {});
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: const Color(0xFF381E72),
+                        ),
+                        child: const Text('Add',
+                            style: TextStyle(color: Colors.white))),
+                ]),
+              );
+            },
+          ),
         ),
-      ));
+        floatingActionButton: selectedTags.isEmpty == false
+            ? FloatingActionButton.extended(
+                onPressed: () {
+                  // Send dato to provider}
+                  // Map data only saves the id of the tags
+                  Map data = {
+                    'favorite_tags': selectedTags,
+                  };
+
+                  Provider.of<Register>(context, listen: false)
+                      .uploadFavoriteTags(data, context);
+                },
+                label: const Text('Save'),
+                icon: const Icon(CupertinoIcons.checkmark_alt),
+                backgroundColor: const Color(0xFF381E72),
+              )
+            : null,
+      );
     }
   }
 }
--- lib/services/reg.dart
+++ lib/services/reg.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart' as Dio;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:snapsnap/main.dart';
 import 'package:snapsnap/models/user.dart';
 import 'package:snapsnap/screens/register/register_password_screen.dart';
 import 'package:snapsnap/screens/register/register_profilephoto_screen.dart';
@@ -27,7 +28,7 @@ class Register extends ChangeNotifier {
   void verifyEmail(Map data, BuildContext context) async {
     try {
       Dio.Response response = await dio().post('/register/email', data: data);
-      print(response.data.toString());
+      // print(response.data.toString());
       // Verify if response  is status 200 or 401
       if (response.statusCode == 200) {
         _user.email = data['email'];
@@ -41,7 +42,7 @@ class Register extends ChangeNotifier {
         notifyListeners();
       }
     } catch (e) {
-      print(e);
+      // print(e);
       _emailStatus = true;
       notifyListeners();
     }
@@ -51,7 +52,7 @@ class Register extends ChangeNotifier {
     try {
       Dio.Response response =
           await dio().post('/register/username', data: data);
-      print(response.data.toString());
+      // print(response.data.toString());
       // Verify if response  is status 200 or 401
       if (response.statusCode == 200) {
         _user.username = data['username'];
@@ -64,7 +65,7 @@ class Register extends ChangeNotifier {
         notifyListeners();
       }
     } catch (e) {
-      print(e);
+      // print(e);
       _usernameStatus = true;
       notifyListeners();
     }
@@ -84,6 +85,37 @@ class Register extends ChangeNotifier {
     }
   }
 
+  void sendUserDataToApi(BuildContext context) async {
+    try {
+      // Send data to API
+      Dio.Response response =
+          await dio().post('/register', data: _user.toJson());
+      if (response.statusCode == 200) {
+        // print response
+        // Save token in secure storage
+        _token = response.data.toString();
+        storeToken(token: _token);
+        notifyListeners();
+        // Next screen to upload profile photo
+        // Navigator.of(context).pushReplacement(
+        //   MaterialPageRoute(builder: (context) => MyApp()),
+        // );
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const RegisterProfilePhotoScreen()));
+      } else {
+        // print(response.data);
+      }
+    } catch (e) {
+      // Manage exceptions in the request
+      // print('Exception: $e');
+    }
+  }
+
+  void storeToken({required String token}) async {
+    storage.write(key: 'token', value: token);
+    notifyListeners();
+  }
+
   void uploadProfilePhoto(FormData formData, BuildContext context) async {
     _token = await storage.read(key: 'token') ?? '';
 
@@ -97,42 +129,28 @@ class Register extends ChangeNotifier {
         MaterialPageRoute(builder: (context) => TagSelectScreen()),
       );
     } catch (e) {
+      // print(e);
+    }
+  }
+
+  void uploadFavoriteTags(Map data, BuildContext context) async {
+    _token = await storage.read(key: 'token') ?? '';
+    // Extract the ids from each tag object and create a new list containing only the ids
+    // Extract the ids from each tag object and create a new list containing only the ids
+    List<int> tags = [];
+    for (var i = 0; i < data['favorite_tags'].length; i++) {
+      tags.add(data['favorite_tags'][i]['id']);
+    }
+    Map<String, dynamic> tagsData = {'favorite_tags': tags};
+    try {
+      Dio.Response response = await dio().post('/favorite_tags',
+          data: tagsData,
+          options: Dio.Options(headers: {'Authorization': 'Bearer $_token'}));
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => MyApp()),
+      );
+    } catch (e) {
       print(e);
     }
-  }
-
-  void sendUserDataToApi(BuildContext context) async {
-    try {
-      // Send data to API
-      Dio.Response response =
-          await dio().post('/register', data: _user.toJson());
-      if (response.statusCode == 200) {
-        // print response
-        print(response.toString());
-        // Save token in secure storage
-        _token = response.data.toString();
-        print(_token);
-        storeToken(token: _token);
-        print(storage.read(key: 'token'));
-        notifyListeners();
-        // Next screen to upload profile photo
-        // Navigator.of(context).pushReplacement(
-        //   MaterialPageRoute(builder: (context) => MyApp()),
-        // );
-        Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => const RegisterProfilePhotoScreen()));
-      } else {
-        print('Error: ${response.statusCode}');
-        print(response.data);
-      }
-    } catch (e) {
-      // Manage exceptions in the request
-      print('Exception: $e');
-    }
-  }
-
-  void storeToken({required String token}) async {
-    storage.write(key: 'token', value: token);
-    notifyListeners();
   }
 }
