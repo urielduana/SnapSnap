ff6b9b3316bee5a60c5c300204eaa929ab8ae65e
urielduana <uri250602@outlook.es>
2023-08-01 21:52:07 -0600

    Update bottom_navigation_bar_screen.dart

--- lib/screens/bottom_navigation_bar_screen.dart
+++ lib/screens/bottom_navigation_bar_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
-import 'dart:io';
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
