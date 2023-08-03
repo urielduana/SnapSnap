d9adc55d70dc4ec56ab4d795d598cc6a356c9972
Angel Montoya <j.gustavo.m245@gmail.com>
2023-08-02 23:06:09 -0600

    add route - post img

--- lib/screens/floating_action_button_camera.dart
+++ lib/screens/floating_action_button_camera.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:snapsnap/screens/gallery/gallery_selector.dart';
+import 'package:snapsnap/screens/Post/post.dart';
+import 'dart:io';
 
 class FloatingActionButtonCamera extends StatelessWidget {
-  const FloatingActionButtonCamera({super.key});
+  const FloatingActionButtonCamera({Key? key}) : super(key: key);
+
+  Future<void> _pickImage(BuildContext context) async {
+    File? selectedImage = await Navigator.push<File?>(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const GallerySelectorScreen(
+          selectedImages: [],
+        ),
+      ),
+    );
+
+    if (selectedImage != null) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Post(selectedImage: selectedImage),
+          ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       backgroundColor: const Color(0xFF381E72),
       onPressed: () {
-        // Upload post route
+        _pickImage(context);
       },
       child: const Icon(
         CupertinoIcons.add,
