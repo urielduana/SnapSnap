c27e26589772d7d19746d8e71c6ff91672c4ce62
urielduana <uri250602@outlook.es>
2023-08-16 11:52:23 -0600

    Search & follow implementation

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -15,17 +15,14 @@ class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController _searchController = TextEditingController();
   List<Map<String, dynamic>> searchResults = [];
 
+  final storage = new FlutterSecureStorage();
   final List<String> profileImgUrls = [
     'https://images.unsplash.com/photo-1524250502761-1ac6f2e30d43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80 ',
     'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
     'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   ];
-
-  final List<String> users = ['Ana', 'Diana', 'Anastacia'];
-  final List<String> usernames = ['@ana', '@diana', '@anastacia'];
-  final List<bool> isFollowing = [false, true, false];
-
-  final storage = new FlutterSecureStorage();
 
   @override
   void dispose() {
@@ -42,12 +39,24 @@ class _SearchScreenState extends State<SearchScreen> {
     return List<Map<String, dynamic>>.from(response.data);
   }
 
-  void followUser(int index) {
+  void followUser(int index) async {
     print('Follow user $index');
+    var token = await storage.read(key: 'token') ?? '';
+    Dio.Response response = await dio().post('/users/follow',
+        data: {'user_id': searchResults[index]['id']},
+        options: Dio.Options(headers: {'Authorization': 'Bearer $token'}));
+    print(response.data);
+    _performSearch(_searchController.text);
   }
 
-  void unfollowUser(int index) {
-    print('Unfollow user $index');
+  void unfollowUser(int index) async {
+    print('Follow user $index');
+    var token = await storage.read(key: 'token') ?? '';
+    Dio.Response response = await dio().post('/users/unfollow',
+        data: {'user_id': searchResults[index]['id']},
+        options: Dio.Options(headers: {'Authorization': 'Bearer $token'}));
+    print(response.data);
+    _performSearch(_searchController.text);
   }
 
   void openProfile(int index) {
@@ -63,7 +72,6 @@ class _SearchScreenState extends State<SearchScreen> {
     }
     try {
       List<Map<String, dynamic>> results = await indexTags(query);
-      print("-----------------");
       print(results);
       setState(() {
         searchResults = results;
@@ -108,75 +116,108 @@ class _SearchScreenState extends State<SearchScreen> {
               ],
             ),
             const SizedBox(height: 20.0),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: searchResults.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 30),
-                            child: Row(
-                              children: [
-                                CircleAvatar(
-                                  backgroundImage:
-                                      NetworkImage(profileImgUrls[index]),
-                                ),
-                                const SizedBox(width: 20),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      searchResults[index]['name'] ??
-                                          'Name unasigned',
-                                      style: TextStyle(
-                                        // fontWeight: FontWeight.w700,
-                                        fontWeight: searchResults[index]
-                                                    ['username'] !=
-                                                null
-                                            ? FontWeight.w200
-                                            : FontWeight.w700,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          (searchResults[index]['username'] !=
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: searchResults.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 30),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage: NetworkImage(searchResults[
+                                            index]['profile_photo'] ??
+                                        'https://ui-avatars.com/api/?name=${searchResults[index]['username']}&background=random&length=1&bold=true'),
+                                  ),
+                                  const SizedBox(width: 20),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        searchResults[index]['name'] ??
+                                            'Name unasigned',
+                                        style: TextStyle(
+                                          // fontWeight: FontWeight.w700,
+                                          fontWeight: searchResults[index]
+                                                      ['username'] !=
                                                   null
-                                              ? '@${searchResults[index]['username']}'
-                                              : 'Username unasigned'),
-                                          style: TextStyle(
-                                            fontWeight: searchResults[index]
-                                                        ['username'] !=
-                                                    null
-                                                ? FontWeight.w400
-                                                : FontWeight.w200,
-                                            fontSize: 13,
-                                          ),
+                                              ? FontWeight.w200
+                                              : FontWeight.w700,
+                                          fontSize: 14,
                                         ),
-                                      ],
-                                    ),
-                                  ],
-                                )
-                              ],
+                                      ),
+                                      const SizedBox(height: 4),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            (searchResults[index]['username'] !=
+                                                    null
+                                                ? '@${searchResults[index]['username']}'
+                                                : 'Username unasigned'),
+                                            style: TextStyle(
+                                              fontWeight: searchResults[index]
+                                                          ['username'] !=
+                                                      null
+                                                  ? FontWeight.w400
+                                                  : FontWeight.w200,
+                                              fontSize: 13,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      // Resto del código para seguir o dejar de seguir
-                    ],
-                  ),
-                );
-              },
+                          ],
+                        ),
+                        if (searchResults[index]['is_following'])
+                          ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(120, 30),
+                              ),
+                              onPressed: () {
+                                unfollowUser(index);
+                              },
+                              icon: const Icon(
+                                CupertinoIcons.checkmark_alt,
+                                size: 15,
+                              ),
+                              label: const Text(
+                                'Following',
+                                style: TextStyle(fontSize: 12),
+                              ))
+                        else
+                          ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(120, 30),
+                                backgroundColor: const Color(0xFF381E72),
+                              ),
+                              onPressed: () {
+                                followUser(index);
+                              },
+                              icon: const Icon(CupertinoIcons.add,
+                                  size: 15, color: Colors.white),
+                              label: const Text('Follow',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12)))
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
